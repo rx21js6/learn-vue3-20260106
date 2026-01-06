e2aabef2ac70d93e14e3b6f2f4eaa440ca14d32b
nauplius developer <nauplius_developer_02@outlook.jp>
2026-01-06 16:58:37 +0900

    step-3〜step-7

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 import Step1 from './tutorial/step-1/Step1.vue'
 import Step2 from './tutorial/step-2/Step2.vue'
+import Step3 from './tutorial/step-3/Step3.vue'
+import Step4 from './tutorial/step-4/Step4.vue'
+import Step5 from './tutorial/step-5/Step5.vue'
+import Step6 from './tutorial/step-6/Step6.vue'
+import Step7 from './tutorial/step-7/Step7.vue'
 </script>
 
 <template>
@@ -16,6 +21,46 @@ import Step2 from './tutorial/step-2/Step2.vue'
     <h2>step-2</h2>
     <label>宣言的レンダリング</label>
     <Step2 />
+  </div>
+
+  <hr />
+
+  <div>
+    <h2>step-3</h2>
+    <label>属性バインディング</label>
+    <Step3 />
+  </div>
+
+  <hr />
+
+  <div>
+    <h2>step-4</h2>
+    <label>イベントリスナー</label>
+    <Step4 />
+  </div>
+
+  <hr />
+
+  <div>
+    <h2>step-5</h2>
+    <label>フォームバインディング</label>
+    <Step5 />
+  </div>
+
+  <hr />
+
+  <div>
+    <h2>step-6</h2>
+    <label>条件付きレンダリング</label>
+    <Step6 />
+  </div>
+
+  <hr />
+
+  <div>
+    <h2>step-7</h2>
+    <label>リストレンダリング </label>
+    <Step7 />
   </div>
 </template>
 
---
+++ src/tutorial/step-4/Step4.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+const onClick = () => {
+  count.value++
+}
+</script>
+
+<template>
+  <!-- make this button work -->
+  <button @click="onClick">Count is: {{ count }}</button>
+</template>
