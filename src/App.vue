de83e940063615154e82cf2758a770adf6eaa1ca
nauplius developer <nauplius_developer_02@outlook.jp>
2026-01-06 14:42:41 +0900

    step-2

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
-import Step1 from './tutorial/step-1/Step1.vue';
-
+import Step1 from './tutorial/step-1/Step1.vue'
+import Step2 from './tutorial/step-2/Step2.vue'
 </script>
 
 <template>
-  <Step1 />
+  <div>
+    <h2>step-1</h2>
+    <label>はじめに</label>
+    <Step1 />
+  </div>
+
+  <hr />
+
+  <div>
+    <h2>step-2</h2>
+    <label>宣言的レンダリング</label>
+    <Step2 />
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
