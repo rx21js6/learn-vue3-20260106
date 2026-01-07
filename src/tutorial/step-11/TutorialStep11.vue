b9f30acaa0386d347a8f6643750a732b812321a8
nauplius developer <nauplius_developer_02@outlook.jp>
2026-01-07 10:08:13 +0900

    step-11

---
+++ src/tutorial/step-11/TutorialStep11.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import TutorialStep11ChildComp from './TutorialStep11ChildComp.vue'
+</script>
+
+<template>
+  <TutorialStep11ChildComp />
+</template>
---
+++ src/tutorial/step-11/TutorialStep11ChildComp.vue
@@ -0,0 +1,4 @@
+<script lang="ts"></script>
+<template>
+  <h2>A Child Component!</h2>
+</template>
