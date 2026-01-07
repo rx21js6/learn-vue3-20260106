38e0bbc185045aaf4b282b75749d5865161848aa
nauplius developer <nauplius_developer_02@outlook.jp>
2026-01-07 11:16:09 +0900

    step-13

---
+++ src/tutorial/step-13/TutorialStep13ChildComp.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const emit = defineEmits(['response', 'response2'])
+
+const sendData = {
+    id: 1,
+    name: 'Vue3',
+}
+
+emit('response', 'Hello from child')
+emit('response2', sendData)
+</script>
+
+<template>
+  <h2>step-13 Child component</h2>
+</template>
