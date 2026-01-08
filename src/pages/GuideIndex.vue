b9c05fdab4466822c11be6c91d4da1d9c7810157
nauplius_developer_02 <nauplius_developer_02@outlook.jp>
2026-01-08 10:26:13 +0900

    Ref, ライフサイクル

--- src/pages/GuideIndex.vue
+++ src/pages/GuideIndex.vue
@@ -2,8 +2,10 @@
 import ConditionalRenderingExample from '@/components/guide/conditiona-rendering/ConditionalRenderingExample.vue'
 import EventHandlingExample from '@/components/guide/event-handling/EventHandlingExample.vue'
 import FormBindingExample from '@/components/guide/form-binding/FormBindingExample.vue'
+import LifecycleExample from '@/components/guide/lifecycle/LifecycleExample.vue'
 import ListRenderingExample from '@/components/guide/list-rendering/ListRenderingExample.vue'
 import ReactivityExample from '@/components/guide/reactivity/ReactivityExample.vue'
+import RefExample from '@/components/guide/ref/RefExample.vue'
 import TemplateSyntaxExample from '@/components/guide/template-syntax/TemplateSyntaxExample.vue'
 </script>
 
@@ -43,5 +45,15 @@ import TemplateSyntaxExample from '@/components/guide/template-syntax/TemplateSy
   <div>
     <h2>リストレンダリング</h2>
     <ListRenderingExample />
+  </div>
+  <hr />
+  <div>
+    <h2>ref</h2>
+    <RefExample />
+  </div>
+  <hr />
+  <div>
+    <h2>ライフサイクル</h2>
+    <LifecycleExample />
   </div>
 </template>
