308db6cb1ed452aade696bf838160bc631cc5454
rx21js6 <67578034+rx21js6@users.noreply.github.com>
2026-01-09 15:59:00 +0900

    バリデーション (#9)
    * VeeValidate導入～Zodでサンプルを作成
    
    * FieldLevelValidation
    
    * FormLevelValidation
    
    * custom-input-01
    
    * validation-timing
    
    ---------
    
    Co-authored-by: nauplius_developer_02 <nauplius_developer_02@outlook.jp>

---
+++ src/components/validation/zod-field-level-validation/ZodFieldLevelValidaton.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { toTypedSchema } from '@vee-validate/zod'
+import { ErrorMessage, Field, Form, useField } from 'vee-validate'
+import { z } from 'zod'
+
+const fieldSchema1 = toTypedSchema(
+  z.string().nonempty('Field is required').email('Must be a valid email'),
+)
+
+// for Composition API
+const fieldSchema2 = toTypedSchema(
+  // z.string().nonempty('Field is required').email('Must be a valid email'),
+  z
+    .string()
+    .min(1, { message: 'Field is required' })
+    .email({ message: 'Must be a valid email' }),
+)
+const { value, errorMessage } = useField('email2', fieldSchema2)
+</script>
+<template>
+  <div>
+    <h3>Zodでフィールドレベルバリデーション</h3>
+  </div>
+  <div>
+    <h4>Field/ErrorMessageコンポーネントを使用</h4>
+    <Form>
+      <Field name="email1" type="email" :rules="fieldSchema1" />
+      <ErrorMessage name="email1" />
+    </Form>
+  </div>
+  <div>
+    <h4>useFieldを使用</h4>
+    <input name="email2" v-model="value" type="email" />
+    <span>{{ errorMessage }}</span>
+  </div>
+</template>
